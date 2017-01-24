1e2c6c10059b8b45da7fc4c1e3e86f200d8a4602
Michael Prather <mprather1@gmail.com>
2017-01-24 14:48:19 +0000

    doesn't show correct answer in data-attribute

--- app/views/AnswerView.js
+++ app/views/AnswerView.js
@@ -2,20 +2,14 @@ var AnswerView = Backbone.Marionette.View.extend({
   tagName: 'li',
   className: 'answer-view',
   template: require("../templates/answer-view-template.html"),
-  attributes: function(){
-    return {
-      "data-correct": this.model.get('correct')
-    }
-  },
   events: {
     'click': 'handleClick'
   },
   handleClick: function(e){
-    console.log($(e.currentTarget).data('correct'))
-    if( $(e.currentTarget).data('correct')){
-      this.$el.css('background-color', 'lightgreen')
+    if(this.model.get('correct')){
+      this.$el.css('background-color', 'lightgreen')      
     } else {
-      this.$el.css('background-color', 'red')
+      this.$el.css('background-color', 'red')      
     }
   }
 });
